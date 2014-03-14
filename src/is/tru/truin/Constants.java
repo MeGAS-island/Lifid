d968ac0653155d163061306f1a09c4428646dfbb
snh11 <snh11@hi.is>
2014-03-14 12:50:04 +0000

    Breytingar varðandi Myndanotendasogu

---
+++ src/is/tru/truin/Constants.java
@@ -0,0 +1,15 @@
+package is.tru.truin;
+
+public class Constants {
+	static int numbImgs=90;
+	public static String[] IMAGES = new String[numbImgs];
+	
+	public static class Config {
+		public static final boolean DEVELOPER_MODE = false;
+	}
+	
+	public static class Extra {
+		public static String IMAGES = "is.tru.truin.IMAGES";
+		public static String IMAGE_POSITION = "is.tru.truin.IMAGE_POSITION";
+	}
+}
